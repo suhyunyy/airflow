d870899b938658b96db0034aa5c1baeb4c0641e5
shkang <shkang991@gmail.com>
2025-03-16 17:19:52 +0900

    python test3

--- dags/dags_empty_operator.py
+++ dags/dags_empty_operator.py
@@ -8,24 +8,21 @@ with DAG(
     start_date=pendulum.datetime(2024, 3, 1, tz="Asia/Seoul"),
     catchup=False,
 ) as dag:
-
     start = EmptyOperator(
         task_id="start"
     )
-
     task_1 = BashOperator(
         task_id="task_1",
         bash_command="echo 'Task 1 실행'"
     )
-
     task_2 = BashOperator(
         task_id="task_2",
         bash_command="echo 'Task 2 실행'"
     )
-
     end = EmptyOperator(
         task_id="end"
     )
-
     # DAG 흐름 설정
     start >> [task_1, task_2] >> end
+
+    
