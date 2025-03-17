7f12cd726c2e57748a8b0471f2e8f4663b3fbd54
shkang <shkang991@gmail.com>
2025-03-17 19:46:52 +0900

    python test933

--- dags/hook_test.py
+++ dags/hook_test.py
@@ -1,7 +1,11 @@
 
-from airflow.hooks.mysql_hook import MySqlHook
+from airflow.hooks.mysql_hook import MySqlHook 
 
 mysql_hook = MySqlHook(mysql_conn_id="my_mysql_conn")
 df = mysql_hook.get_pandas_df(sql="SELECT * FROM users")
 
 print(df)
+
+
+
+
--- plugins/shell/process_csv.sh
+++ plugins/shell/process_csv.sh
@@ -3,6 +3,9 @@
 INPUT_FILE="dags/input_raw.csv"
 OUTPUT_FILE="dags/output_process.csv"
 
+# output_process.csv 파일이 없으면 생성
+touch "$OUTPUT_FILE"
+
 # 헤더 저장
 head -n 1 "$INPUT_FILE" > "$OUTPUT_FILE"
 
