6543eee792c39e4079514ab061208264d5e03601
shkang <shkang991@gmail.com>
2025-03-18 12:04:34 +0900

    python test933

--- dags/bash_data_pre.py
+++ dags/bash_data_pre.py
@@ -4,17 +4,12 @@ from datetime import datetime, timedelta
 
 # 기본 DAG 설정
 default_args = {
-    "owner": "airflow",
-    "depends_on_past": False,
     "start_date": datetime(2025, 3, 1),
-
 }
-
 dag = DAG(
-    "bash_csv_processing_dag",
+    "bash_processing_dag",
     default_args=default_args,
-    description="DAG to process CSV file using Bash script",
-
+    description="DAG to process using Bash script",
 )
 
 # BashOperator Task
@@ -26,5 +21,6 @@ process_csv_task = BashOperator(
 
 # DAG 실행 순서 정의
 process_csv_task
+
 
 
