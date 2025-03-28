df8df997dae1622b11d1c8a641f049858206ba6a
shkang <shkang991@gmail.com>
2025-03-28 19:35:57 +0900

    python test933

--- dags/scripts/spark_wordcount.py
+++ dags/scripts/spark_wordcount.py
@@ -1,18 +1,8 @@
-from pyspark.sql import SparkSession
+from pyspark import SparkContext
 
-if __name__ == "__main__":
-    spark = SparkSession.builder.appName("WordCount").getOrCreate()
-    sc = spark.sparkContext
+sc = SparkContext("spark://spark-master:7077", "SimpleTest")
 
-    data = sc.parallelize([
-        "hello world",
-        "hello airflow",
-        "spark is fast",
-        "docker is cool"
-    ])
+data = sc.parallelize([1, 2, 3, 4, 5])
+squared = data.map(lambda x: x * x).collect()
 
-    words = data.flatMap(lambda line: line.split(" "))
-    wordCounts = words.map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
-    wordCounts.saveAsTextFile("output_wordcount")
-
-    spark.stop()
+print("Squared numbers:", squared)
